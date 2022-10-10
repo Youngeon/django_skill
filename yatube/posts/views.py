5e05e2b0e96ebe5fe7eb1718fd6409fa35997ea4
Youngeon <emik219@gmail.com>
2022-10-10 18:42:46 +0000

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):    
+    return HttpResponse('Главная страница')
+
+
+# Страница со группами
+def group_posts(request,slug):
+    return HttpResponse(f'Группы {slug}')
+
+
+# Страница с информацией об одном сорте мороженого;
+# view-функция принимает параметр pk из path()
+#def ice_cream_detail(request, pk):
+    #return HttpResponse(f'Мороженое номер {pk}') 
